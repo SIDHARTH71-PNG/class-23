cf99117baaa645ae1a23ca44c8a59aa04af15683
SIDHARTH71-PNG <69514690+SIDHARTH71-PNG@users.noreply.github.com>
2020-09-01 17:56:44 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':2.0,
+        'density':1.8
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle=this.body.angle;
+
+    push ();
+    rectMode(CENTER);
+    angleMode(RADIANS);
+    rotate (angle);
+    fill(255);
+    translate(pos.x, pos.y);
+    rect(0,0, this.width, this.height);
+    pop ();
+   
+  }
+};
